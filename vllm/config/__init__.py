2b712cc272ec4f15276655f14327777981811fe8
Jiang Wu <JWu@cclgroup.com>
2025-09-23 15:50:01 -0700

    Adding in leng's fix

--- vllm/config/__init__.py
+++ vllm/config/__init__.py
@@ -843,7 +843,7 @@ class ModelConfig:
             return
 
         if is_runai_obj_uri(model):
-            object_storage_model = ObjectStorageModel()
+            object_storage_model = ObjectStorageModel(model)
             object_storage_model.pull_files(
                 model, allow_pattern=["*.model", "*.py", "*.json"])
             self.model_weights = model
@@ -861,7 +861,7 @@ class ModelConfig:
 
         # Only download tokenizer if needed and not already handled
         if is_runai_obj_uri(tokenizer):
-            object_storage_tokenizer = ObjectStorageModel()
+            object_storage_tokenizer = ObjectStorageModel(tokenizer)
             object_storage_tokenizer.pull_files(
                 model,
                 ignore_pattern=["*.pt", "*.safetensors", "*.bin", "*.tensors"])
